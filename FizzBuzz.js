90f388bf96080b86094756c02a43e58038a2179e
LordFeexz <anandafiqri1403@gmail.com>
2022-12-13 23:18:32 +0700

    fizzbuzz

---
+++ FizzBuzz.js
@@ -0,0 +1,15 @@
+var fizzBuzz = function (num) {
+    var temp = [];
+    for (var i = 1; i <= num; i++) {
+        if (i % 15 === 0)
+            temp.push('FizzBuzz');
+        else if (i % 3 === 0)
+            temp.push('Fizz');
+        else if (i % 5 === 0)
+            temp.push('Buzz');
+        else
+            temp.push(i);
+    }
+    return temp.join('\n');
+};
+console.log(fizzBuzz(100));
---
+++ FizzBuzz.ts
@@ -0,0 +1,15 @@
+const fizzBuzz = (num : number) => {
+    let temp : any = []
+    for(let i = 1 ; i <= num ; i++){
+        if(i % 15 === 0)
+            temp.push('FizzBuzz')
+        else if(i % 3 === 0)
+            temp.push('Fizz')
+        else if(i % 5 === 0)
+            temp.push('Buzz')
+        else temp.push(i)
+    }
+    return temp.join('\n')
+}
+
+console.log(fizzBuzz(100))
